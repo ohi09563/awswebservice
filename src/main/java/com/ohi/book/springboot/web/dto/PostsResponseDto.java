a1d43ee09c7f7f7cf941e75a0a943f3cee100b01
ohi09563 <ohi09563@gmail.com>
2022-06-02 22:19:43 +0900

    jpa 연습 페이지 112

---
+++ src/main/java/com/ohi/book/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,23 @@
+package com.ohi.book.springboot.web.dto;
+
+import com.ohi.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity){
+
+        this.id= entity.getId();
+        this.title=entity.getTitle();
+        this.content= entity.getContent();
+        this.author= entity.getAuthor();
+
+    }
+
+
+}
